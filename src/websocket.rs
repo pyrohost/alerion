0481bebe49cf3dba1a581d2f733470b524695d6e
Checksum <me@checksum.space>
2024-03-27 18:09:59 -0500

    chore: simple echo server + some libraries we'll need.

---
+++ src/websocket.rs
@@ -0,0 +1,21 @@
+use actix::StreamHandler;
+
+use actix::Actor;
+use actix_web_actors::ws;
+
+pub struct WebSocket;
+
+impl Actor for WebSocket {
+    type Context = ws::WebsocketContext<Self>;
+}
+
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebSocket {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        match msg {
+            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
+            Ok(ws::Message::Text(text)) => ctx.text(text),
+            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            _ => (),
+        }
+    }
+}
