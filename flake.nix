0c5a07e37badcd1510a0dbfe959056e6047e459a
pupbrained <mars@pupbrained.xyz>
2024-04-19 03:26:30 -0400

    build: add flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "DevShell for Alerion";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = [
+              openssl
+              pkg-config
+              (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+              (rust-bin.nightly."2024-04-19".rustfmt)
+            ];
+          };
+        }
+    );
+}
