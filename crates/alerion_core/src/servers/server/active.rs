39e54bd1a1818c249ab5961c140a9f2b0aae96f7
fetch <108630700+fetchfern@users.noreply.github.com>
2024-05-23 21:08:43 -0400

    chore: `Server` revamp p2

---
+++ crates/alerion_core/src/servers/server/active.rs
@@ -0,0 +1,31 @@
+pub enum State {
+    Installing,
+    Installed,
+}
+
+#[derive(Debug, Clone, Copy, Default)]
+pub struct Egg;
+
+pub struct ActiveServer {
+    egg: Egg,
+    state: State,
+}
+
+impl ActiveServer {
+    pub fn with_egg(egg: Egg) -> ActiveServer {
+        let server = ActiveServer {
+            egg,
+            state: State::Installing,
+        };
+
+        server
+    }
+    
+    /// Begins a reinstallation task.
+    pub fn reinstall(&self) {
+    }
+
+    /// Forces an uninstallation task.
+    pub fn force_uninstall(&self) {
+    }
+}
