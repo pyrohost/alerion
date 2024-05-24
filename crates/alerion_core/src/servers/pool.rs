2972db05f0ef7e4bb120c5f999dd408d8267ef7a
fetch <108630700+fetchfern@users.noreply.github.com>
2024-05-24 01:44:27 -0400

    chore: use new remote api in serverpool

--- crates/alerion_core/src/servers/pool.rs
+++ crates/alerion_core/src/servers/pool.rs
@@ -5,25 +5,25 @@ use tokio::sync::RwLock;
 use uuid::Uuid;
 
 use crate::configuration::AlerionConfig;
-use crate::servers::server::{ServerInfo, Server};
+use crate::servers::server::Server;
 use crate::servers::{docker, remote, ServerError};
 
 pub struct ServerPool {
     servers: RwLock<HashMap<Uuid, Arc<Server>>>,
-    remote_api: Arc<remote::RemoteClient>,
+    remote_api: remote::Api,
     docker: Arc<Docker>,
 }
 
 impl ServerPool {
     pub async fn new(config: &AlerionConfig) -> Result<Self, ServerError> {
-        let remote_api = remote::RemoteClient::new(config)?;
+        let remote_api = remote::Api::new(config)?;
 
         tracing::info!("initiating connection to Docker Engine");
         let docker = Docker::connect_with_defaults().map_err(docker::DockerError::Api)?;
 
         Ok(Self {
             servers: RwLock::new(HashMap::new()),
-            remote_api: Arc::new(remote_api),
+            remote_api,
             docker: Arc::new(docker),
         })
     }
@@ -37,17 +37,9 @@ impl ServerPool {
         for s in servers {
             tracing::info!("recovering server {}", s.uuid);
 
-            let uuid = s.uuid;
-            let info = ServerInfo::from_remote_info(s.settings);
-            let server = Server::new(
-                uuid,
-                info,
-                Arc::clone(&self.remote_api),
-                Arc::clone(&self.docker),
-            )
-            .await?;
+            let _uuid = s.uuid;
 
-            self.servers.write().await.insert(uuid, server);
+            tracing::error!("!!!!!!!!!TODO: RECOVERING SERVERS");
         }
 
         Ok(())
@@ -55,17 +47,12 @@ impl ServerPool {
 
     #[tracing::instrument(name = "create_server", skip(self))]
     pub async fn create(&self, uuid: Uuid, start_on_completion: bool) -> Result<Arc<Server>, ServerError> {
-        let remote_api = Arc::clone(&self.remote_api);
         let docker = Arc::clone(&self.docker);
 
-        tracing::debug!("fetching server configuration from remote");
-        let config_fut = remote_api.get_server_configuration(uuid);
-        let config = crate::ensure!(config_fut.await, "failed to retrieve server configuration from remote source");
+        let server = Server::new(uuid, self.remote_api.clone(), docker);
+        let server = Arc::new(server);
 
-        let server_info = ServerInfo::from_remote_info(config.settings);
-
-        let server = Server::new(uuid, server_info, remote_api, docker).await?;
-        self.servers.write().await.insert(uuid, Arc::clone(&server));
+        tracing::error!("!!!!!!!!!TODO: ACTUALLY ADDING SERVERS AND STUFF");
 
         Ok(server)
     }
