5ecf26ccc1c5079b9a352e4238041cb63e8c1e6a
fetch <108630700+fetchfern@users.noreply.github.com>
2024-05-29 04:57:06 -0400

    chore(docker): no more volumes

--- crates/alerion_core/src/docker/models.rs
+++ crates/alerion_core/src/docker/models.rs
@@ -24,11 +24,9 @@ pub enum Inspected<T: Inspectable> {
     None,
 }
 
-pub use volume::{VolumeName, Volume};
 pub use container::{Container, ContainerName};
 pub use bind_mount::BindMount;
 
-pub mod volume;
 pub mod container;
 pub mod bind_mount;
 pub mod network;
