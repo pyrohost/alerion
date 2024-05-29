fb1f367e3baf9d4abca111f4fab165978319f27a
fetch <108630700+fetchfern@users.noreply.github.com>
2024-05-29 03:37:35 -0400

    chore: run/cleanup script

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
-cargo build && sudo ./target/debug/alerion_cli
+#!/bin/sh
+
+cargo build
+if [ $? ]; then
+	export RUST_LOG=debug
+	export RUST_BACKTRACE=full
+	target/debug/alerion_cli
+	docker container prune --force
+	rm -r "$(jq -Mr '.data_dir' < $HOME/.config/alerion/config.json)"
+fi
